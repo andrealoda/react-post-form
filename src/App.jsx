c16f6ec169508de9dceb82d13c68fa4c8b9c1290
andrea8980 <andrea@casaloda.it>
2026-05-06 15:55:07 +0200

    set up app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
 import { useState } from 'react'
 
-function App() {
+const emptyPost = {
+  author: '',
+  title: '',
+  comapnyName: '',
+  body: '',
+  public: false
+}
+
+
+
+const App = () => {
+
+  const [postData, setPostData] = useState(emptyPost);
+
+  function handlePostData(e) {
+    console.log(e);
+    setPostData(
+      {
+        ...postData,
+        [e.target.name]: e.target.value
+      }
+    )
+  }
 
   return (
     <>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <PostNewPost postData={postData} onPostData={handlePostData} />
+          </div>
+          <div className="col">
+            <ShowPost postData={postData} />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
