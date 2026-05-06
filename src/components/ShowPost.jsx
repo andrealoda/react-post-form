b9a7505800e685538be69ac6afe8488bac4788a8
andrea8980 <andrea@casaloda.it>
2026-05-06 16:00:00 +0200

    set a draft for ShowPost.jsx

---
+++ src/components/ShowPost.jsx
@@ -0,0 +1,10 @@
+export default function ShowPost({ postData }) {
+    return (
+        <>
+            <div className="author">{postData.author}</div>
+            <div className="author">{postData.title}</div>
+            <div className="author">{postData.body}</div>
+            <div className="public">{false}</div>
+        </>
+    )
+}
